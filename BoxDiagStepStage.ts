5f09cab3dc6f904b4af5417bb33cc1ad25696ef6
Anwesh43 <anweshthecool0@gmail.com>
2018-11-10 13:50:59 +0530

    adding BoxDiagStep class

--- BoxDiagStepStage.ts
+++ BoxDiagStepStage.ts
@@ -150,3 +150,27 @@ class BDSNode {
         return this
     }
 }
+
+class BoxDiagStep {
+
+    root : BDSNode = new BDSNode(0)
+    curr : BDSNode = this.root
+    dir : number = 1
+    
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
